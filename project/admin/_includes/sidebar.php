ef076ddc9baa3ba9986b42a05196ab7a2c56849e
Ren2908 <phambaduong2809@gmail.com>
2025-11-24 03:07:18 +0700

    cập nhật logo

--- project/admin/_includes/sidebar.php
+++ project/admin/_includes/sidebar.php
@@ -3,9 +3,9 @@
     <?php if (isset($_SESSION['ma_quyen']) && $_SESSION['ma_quyen'] == 'Q1'): ?>
         <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index_admin.php?page=dashboard">
             <div class="sidebar-brand-icon rotate-n-15">
-                <i class="fas fa-laugh-wink"></i>
+                <!-- <i class="fas fa-laugh-wink"></i> -->
             </div>
-            <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+            <div class="sidebar-brand-text mx-3">3TL Admin <sup>2</sup></div>
         </a>
 
         <!-- Divider -->
