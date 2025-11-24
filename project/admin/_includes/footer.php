73d6d04bb3d80d3ad518a1427e1c27c380068bc2
Ren2908 <phambaduong2809@gmail.com>
2025-11-25 00:19:07 +0700

    xoá _includes thay _includes_admin

--- project/admin/_includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="sticky-footer bg-white">
-    <div class="container my-auto">
-        <div class="copyright text-center my-auto">
-            <span>Được làm bởi thành viên Phạm Bá Dương</span>
-        </div>
-    </div>
-</footer>
